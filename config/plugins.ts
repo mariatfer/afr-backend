47ca77b77027421f3408ca62e0049f3acc7248aa
María Fernández <matefe1706@gmail.com>
2025-10-08 11:01:40 +0200

    feat(config): add resend plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,18 @@
-export default () => ({});
+export default ({
+  env,
+}: {
+  env: (key: string, defaultValue?: string) => string;
+}) => ({
+  email: {
+    config: {
+      provider: "strapi-provider-email-resend",
+      providerOptions: {
+        apiKey: env("RESEND_API_KEY"),
+      },
+      settings: {
+        defaultFrom: env("RESEND_DEFAULT_FROM"),
+        defaultReplyTo: env("RESEND_DEFAULT_REPLY_TO"),
+      },
+    },
+  },
+});
